e45f0c9fdc188a833e7db1a99d01cefbfa19c863
Lander Debeir <landerdebeir@outlook.com>
2026-04-10 16:06:29 +0200

    create basic app structure

---
+++ src/Infrastructure/Api/modules/mod.ts
@@ -0,0 +1,6 @@
+//Modules
+export {AppModule} from './app/app.module';
+//Controllers
+export {AppController} from './app/app.controller';
+//Services
+export {AppService} from './app/app.service';
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './Infrastructure/Api/modules/mod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
