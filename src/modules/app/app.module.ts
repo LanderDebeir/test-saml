de212712f273d880da6c52dd4a0806f884206710
Lander Debeir <landerdebeir@outlook.com>
2026-04-21 09:27:04 +0200

    Remove mod file

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController, AppService } from '../mod';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [],
--- src/modules/mod.ts
+++
@@ -1,6 +0,0 @@
-//Modules
-export {AppModule} from './app/app.module';
-//Controllers
-export {AppController} from './app/app.controller';
-//Services
-export {AppService} from './app/app.service';
