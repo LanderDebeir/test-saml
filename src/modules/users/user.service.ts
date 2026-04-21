a3858e65d7a1d51717350f3e70610422d5ea670f
Lander Debeir <landerdebeir@outlook.com>
2026-04-21 13:39:46 +0200

    Add crud operations to user service

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -28,4 +28,46 @@ export class UserService {
             return null;
         }
     }
+
+    async createUser({email, name, password}: {email: string, name: string, password: string}) {
+        try{
+            return this.prisma.user.create({
+                data: {
+                    email,
+                    name,
+                    password
+                }
+            });
+        } catch (error) {
+            this.logger.error("Error creating user", error);
+            throw new Error("Failed to create user");
+        }
+    }
+
+    async deleteUser({id}: {id: number}) {
+        try{
+            return this.prisma.user.delete({
+                where: {id}
+            });
+        } catch (error) {
+            this.logger.error("Error deleting user", error);
+            throw new Error("Failed to delete user");
+        }
+    }
+
+    async updateUser({id, email, name, password}: {id: number, email?: string, name?: string, password?: string}) {
+        try{
+            return this.prisma.user.update({
+                where: {id},
+                data: {
+                    email,
+                    name,
+                    password
+                }
+            });
+        } catch (error) {
+            this.logger.error("Error updating user", error);
+            throw new Error("Failed to update user");
+        }
+    }
 }
