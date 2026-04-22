937ec7b1f6a8a38af26d85e217748dc9a97b14e6
Lander Debeir <landerdebeir@outlook.com>
2026-04-22 13:21:31 +0200

    Remove app controller and app service

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SamlModule } from '../saml/saml.module';
-import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [
      SamlModule
     ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/modules/saml/saml.controller.ts
+++ src/modules/saml/saml.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from "@nestjs/common";
 import { SamlService } from "./saml.service";
 
-@Controller({path: "auth", version: "1"})
+@Controller({version: "1"})
 export class SamlController {
     constructor(
         private readonly samlService: SamlService
