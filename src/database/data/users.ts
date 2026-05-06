f38c91a02eb86d164d62f2fdf49ffeddb97961ef
Lander Debeir <landerdebeir@outlook.com>
2026-05-06 11:12:24 +0200

    Remove id from seeder data

--- src/database/data/users.ts
+++ src/database/data/users.ts
@@ -2,14 +2,12 @@ import { hashPassword } from 'src/utils';
 import 'dotenv/config';
 
 export const users: {
-  id: number;
   email: string;
   displayName: string;
   password: string;
   createdAt: Date;
 }[] = [
   {
-    id: 1234,
     email: process.env.TEST_USER_EMAIL || '',
     displayName: process.env.TEST_USER_NAME || '',
     password: hashPassword(process.env.TEST_USER_PASSWORD || ''),
